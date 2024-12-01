f723559c14b5afeaba0566bd423e824b247054bf
Timur <61709763+cuteUtya@users.noreply.github.com>
2024-12-01 01:15:52 +0100

    cors support

--- back/index.js
+++ back/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const expressWs = require('express-ws');
 const bodyParser = require('body-parser');
 const uuidv4 = require("uuid").v4;
+const cors = require('cors');
 
 const app = express();
+app.use(cors());
 expressWs(app);
 app.use(bodyParser.json());
 
